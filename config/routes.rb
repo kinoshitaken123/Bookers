ab35abd9b0946004fc9ac468b3adfd931876f227
EC2 Default User <ec2-user@ip-172-31-2-117.ec2.internal>
2021-05-02 13:58:49 +0000

    3commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,5 @@
 Rails.application.routes.draw do
 root to: 'homes#top'
+resources :books
 
- get 'books/new'
- get 'top' =>'homes#top'
- get 'books' =>'books#index'
- post 'books' => 'books#create'
- get 'books/:id' => 'books#show', as: 'book'
- get 'books/:id/edit' => 'books#edit', as: 'edit_book'
- patch 'books/:id' => 'books#update', as: 'update_book'
- put 'books/:id' => 'books#update'
- delete 'books/:id' => 'books#destroy'
-
- # post'books' => 'books#create'
- # get 'books' => 'books#index'
- # get 'books/:id' => 'books#show',as: 'book'
- # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
- # patch 'books/:id' =>'books#update',as: 'update_book'
- # delete 'books/:id' =>'books#destroy',as: 'destroy_todolist'
 end
