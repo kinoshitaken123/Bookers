1bb7cf1f694dfe8502330b2ec1fb17a2b5bf003a
EC2 Default User <ec2-user@ip-172-31-2-117.ec2.internal>
2021-05-03 11:09:29 +0000

    4commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
 root to: 'homes#top'
 resources :books
 
+#   get 'book' => 'book#top'
+#   post 'books' => 'books#create'
+#   get 'books' => 'books#index'
+#   get 'books/:id' => 'books#show', as: 'book'
+#   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+#   patch 'books/:id' => 'books#update', as: 'update_book'
+
+
 end
